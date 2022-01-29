1ddd476627ef360ee028acdb8736d8060f0701df
robialakbar <robialakbar@gmail.com>
2022-01-30 01:19:46 +0700

    Migrate

---
+++ app/Models/CrudModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CrudModel extends Model
+{
+    use SoftDeletes;
+    protected $table = 'product';
+    protected $fillable = [
+        'name'
+    ];
+    protected $hidden;
+}
